01b193973bbc2a4e24bcf669109cf4d9f0fa16c6
Mrinal Pramanik <mrinalpramanik98@gmail.com>
2020-07-05 11:31:37 +0530

    Python 3

---
+++ Exceptions.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+for i in range(n):
+    a,b=input().split()
+    try:
+        print(int(a)//int(b))
+    except Exception as e:
+        print("Error Code: "+str(e))
